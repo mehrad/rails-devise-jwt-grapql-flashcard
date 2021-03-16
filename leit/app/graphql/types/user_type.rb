955d51210353d66408ec2f2593cc3b5bc9186094
Mahrad <hossein.ataeefard@gmail.com>
2021-03-16 14:17:24 +0330

    Add base 64 user authenication for current user

--- leit/app/graphql/types/user_type.rb
+++ leit/app/graphql/types/user_type.rb
@@ -25,6 +25,15 @@ module Types
     field :authentication_token_created_at, GraphQL::Types::ISO8601DateTime, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def authentication_token
+      if object.gql_id != context[:current_user]&.gql_id
+        raise GraphQL::UnauthorizedFieldError,
+              "Unable to access authentication_token"
+      end
+
+      object.authentication_token
+    end
   end
 end
 
