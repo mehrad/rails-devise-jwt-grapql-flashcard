0151c5a77777280d3c78a7867e81625879067147
Mahrad <hossein.ataeefard@gmail.com>
2021-05-08 18:44:37 +0430

    Set Cords

--- leit/config/initializers/cors.rb
+++ leit/config/initializers/cors.rb
@@ -9,10 +9,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3000'
 
     resource '*',
              headers: :any,
+             expose: %w[access-token expiry token-type Authorization],
+             credentials: true,
              methods: %i[get post put patch delete options head]
   end
 end
